0c25c991852dc904c430872f4bdd5ce1901d2492
larryQuao <honyadjilawrence2s1@gmail.com>
2025-09-29 22:53:42 +0000

    feat: Add alx-project-2 Next.js application

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a sample Next.js application.</p>
+    </div>
+  );
+}
+
+export default Home;
